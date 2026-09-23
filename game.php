eea94aa876dacc69e167fd80616c8839ca42f556
unknown <seowzhiqi5@gmail.com>
2026-09-23 16:31:04 +0800

    edit game.php4

--- game.php
+++ game.php
@@ -32,7 +32,7 @@ $enemyModelPath = 'assets/models/enemies/' . $enemyModel;
 
     <!-- AR 识别容器 -->
     <div id="ar-container">
-        <a-scene mindar-image="imageTargetSrc: ./targets.mind?v=<?php echo time(); ?>; autoStart: true;" 
+        <a-scene mindar-image="imageTargetSrc: ./assets/targets/targets.mind?v=<?php echo time(); ?>; autoStart: true;" 
                  embedded 
                  color-space="sRGB" 
                  renderer="colorManagement: true, physicallyCorrectLights" 
